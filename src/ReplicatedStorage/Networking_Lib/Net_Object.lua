07515858c888014336e3acbe1bfd1cb4b62eb641
SxtTxch <116434377+SxtTxch@users.noreply.github.com>
2024-09-17 20:47:37 +0200

    Update Net_Object.lua >> snake_case
    Update Net_Object.lua >> snake_case

--- src/ReplicatedStorage/Networking_Lib/Net_Object.lua
+++ src/ReplicatedStorage/Networking_Lib/Net_Object.lua
@@ -3,95 +3,92 @@ local RunService = game:GetService("RunService")
 local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Networking_Lib = script.Parent
-local Remotes = require(Networking_Lib.Remotes)
+local networking_lib = script.Parent
+local remotes = require(networking_lib.Remotes)
 
-local Connector = Networking_Lib.Connector
+export type net_interface_impl = {
+    __index: net_interface_impl,
 
--- Define the type for netInterfaceType
-export type netInterfaceType = {
-	__index: netInterfaceType,
-	
-	new: (name: string) -> netInterfaceType,
-	get: (name: string) -> netInterfaceType?,
-	
-	fireServer: (self: netInterfaceType, ... any) -> (),
-	fireClient: (self: netInterfaceType, client : Player, ... any) -> (),
-	fireAllClients: (self: netInterfaceType, ... any) -> (),
-	
-	onServer: (self: netInterfaceType, () -> any?) -> (),
-	onClient: (self: netInterfaceType, () -> any?) -> (),
-	
-	Destroy: (self: netInterfaceType) -> (),
-	name: string,
-	remote : RemoteEvent,
+    new: (name: string) -> net_interface_impl,
+    get: (name: string) -> net_interface_impl?,
+
+    fire_server: (self: net_interface_impl, ... any) -> (),
+    fire_client: (self: net_interface_impl, client: Player, ... any) -> (),
+    fire_all_clients: (self: net_interface_impl, ... any) -> (),
+
+    on_server: (self: net_interface_impl, callback: () -> any?) -> (),
+    on_client: (self: net_interface_impl, callback: () -> any?) -> (), 
+
+    destroy: (self: net_interface_impl) -> (),
+    name: string,
+    remote: RemoteEvent,
 }
 
-local netContent: {[string]: netInterfaceType} = {}
+local net_interfaces: {[string]: net_interface_impl} = {}
 
-local netInterface: netInterfaceType = {} :: netInterfaceType
-netInterface.__index = netInterface
+local net_interface: net_interface_impl = {} :: net_interface_impl
+net_interface.__index = net_interface
 
-function netInterface.new(name: string): netInterfaceType
-	
-	for existingName ,_ in netContent do
-		if existingName == name then
-			return netContent[name]
-		end
-	end
-	
-	local netInterfaceObject = setmetatable({}, netInterface) :: any
+function net_interface.new(name: string): net_interface_impl
+    if net_interfaces[name] then
+        return net_interfaces[name]
+    end
 
-	netInterfaceObject.name = name
+    local new_interface = setmetatable({}, net_interface) :: net_interface_impl
+    new_interface.name = name
+    net_interfaces[name] = new_interface
 
-	netContent[name] = netInterfaceObject
-	
-	if RunService:IsClient() then
-		local object = Connector:InvokeServer(name)
-		netInterfaceObject = setmetatable(object, netInterface) :: netInterfaceType
-	end
-	
-	netInterfaceObject.remote = Remotes.newRemote(name)
-	
-	return netInterfaceObject
+    if RunService:IsClient() then
+        local object = Connector:InvokeServer(name)
+        new_interface = setmetatable(object, net_interface) :: net_interface_impl
+    end
+
+    new_interface.remote = remotes.new_remote(name)
+
+    return new_interface
+end
+
+function net_interface.get(name: string): net_interface_impl?
+    return net_interfaces[name]
+end
+
+function net_interface:fire_server(...)
+    self.remote:FireServer(...)
+end
+
+function net_interface:fire_client(client: Player, ...)
+    self.remote:FireClient(client, ...)
+end
+
+function net_interface:fire_all_clients(...)
+    self.remote:FireAllClients(...)
+end
+
+function net_interface:destroy()
+    self.remote:Destroy()
+    net_interfaces[self.name] = nil
+end
+
+function net_interface:on_server(callback: () -> any?)
+    if RunService:IsClient() then
+        warn("net_interface:on_server() should be used only on the server! Please use net_interface:on_client()")
+        return
+    end
+    self.remote.OnServerEvent:Connect(callback)
+end
+
+function net_interface:on_client(callback: () -> any?)
+    if RunService:IsServer() then
+        warn("net_interface:on_client() should be used only on the client! Please use net_interface:on_server()")
+        return
+    end
+    self.remote.OnClientEvent:Connect(callback)
 end
 
 if RunService:IsServer() then
-	Connector.OnServerInvoke = function(_, name : string)
-		local newInterface = netInterface.new(name)
-		return newInterface
-	end
+    Connector.OnServerInvoke = function(_, name: string)
+        return net_interface.new(name)
+    end
 end
 
-function netInterface.get(name: string): netInterfaceType?
-	return netInterface.new(name) or nil
-end
-
-function netInterface:fireServer(...)
-	self.remote:FireServer(...)
-end
-
-function netInterface:fireClient(client, ...)
-	self.remote:FireClient(client, ...)
-end
-
-function netInterface:fireAllClients(...)
-	self.remote:FireAllClients(...)
-end
-
-function netInterface:Destroy()
-	self.remote:Destroy()
-	netContent[self.name] = nil
-end
-
-function netInterface:onServer(callback)
-	if RunService:IsClient() then warn("netInterface.onServer() should be used only on the server! Please use netInterface.onClient()") return end
-	self.remote.OnServerEvent:Connect(callback)
-end
-
-function netInterface:onClient(callback)
-	if RunService:IsServer() then warn("netInterface.onClient() should be used only on the client! Please use netInterface.onServer()") return end
-	self.remote.OnClientEvent:Connect(callback)
-end
-
-return netInterface
+return net_interface
